51f1b1a149b1de3514c757e4ec7f6bb8ac77edcd
Vigh Iosif <vighiosif@gmail.com>
2016-10-11 12:01:25 +0000

    added alias for going to online repo

--- aliases.bash
+++ aliases.bash
@@ -28,6 +28,7 @@ alias checkout-development='git checkout development && git remote update && git
 alias gl='git log --pretty=format:"[%C(auto)%h][%Cgreen%an%Creset]%C(auto)%d %s (%Cblue%ar%Creset)" --graph --color -8'
 alias glall='git log --pretty=format:"[%C(auto)%h][%Cgreen%an%Creset]%C(auto)%d %s (%Cblue%ar%Creset)" --graph --color'
 alias git-ignore='git ls-files --others -i --exclude-standard'
+alias git-online='open $(git config --get remote.origin.url)'
 
 
 #===============================================
