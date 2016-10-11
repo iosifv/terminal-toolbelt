f825ba2bb8796fe5c6991cf3c3d164e419ca79b9
Iosif Vigh <vighiosif@gmail.com>
2016-10-11 21:26:15 +0200

    merged from bitbucket

--- functions.bash
+++ functions.bash
@@ -1,26 +1,38 @@
 #===============================================
 ######## Git ===================================
 #===============================================
-function push-origin {
+function gp {
   git add .
   git commit -m "$2"
   git push origin $1
   echo "------"
   git status
 }
- 
+
+function gp-master {
+  git add .
+  git commit -m "$2"
+  git push origin master
+  echo "------"
+  git status
+}
+
  # Which is the branch I am on right now:
  # git rev-parse --abbrev-ref HEAD
- 
+
  # What's the hash of my last commit
  # git rev-parse --verify HEAD
 
-function fixup {
+function gfixup {
   git add .
   git commit --fixup $(git rev-parse --verify HEAD)
   git push origin $(git rev-parse --abbrev-ref HEAD)
   echo "------"
   git status
+}
+
+function gc {
+  git commit -m "$1"
 }
 
 
