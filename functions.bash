1cabc4981edbebdf979272a18c5f42b56702dd76
Vigh Iosif <vighiosif@gmail.com>
2016-10-11 09:31:33 +0000

    commit --fixup

--- functions.bash
+++ functions.bash
@@ -8,6 +8,18 @@ function push-origin {
   echo "------"
   git status
 }
+ 
+ # ToDo: find out which branch I am working on and reduce the arguments
+ # git rev-parse --abbrev-ref HEAD
+ 
+# Example usage: fixup my-branch "my comment"
+function fixup {
+  git add .
+  git commit --fixup "$2"
+  git push origin $1
+  echo "------"
+  git status
+}
 
 
 #===============================================
