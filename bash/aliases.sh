26c38314518b9f8d0d8cd44053993bf79b8d2148
Iosif <vighiosif@gmail.com>
2016-10-13 17:11:12 +0200

    restructure2

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -10,3 +10,7 @@ alias sudo="echo '${C_YELLOW}Are you sure?${C_RESET}'"
 alias epoch='date +"%s"'
 alias version='echo "bash version: ${BASH_VERSION}"'
 alias path='echo -e ${PATH//:/\\n}' # nice path printing
+
+# display battery info on your Mac
+# see http://blog.justingreer.com/post/45839440/a-tale-of-two-batteries
+alias battery='ioreg -w0 -l | grep Capacity | cut -d " " -f 17-50'
