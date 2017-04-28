31671ed4928ef2c0d260b3451b5ba89d08361509
Iosif Vigh <vighiosif@gmail.com>
2017-04-28 13:03:17 +0100

    added composer

--- sources/configs.sh
+++ sources/configs.sh
@@ -11,6 +11,7 @@ export CLICOLOR=true # enable colors on OSX terminal
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
+alias composer="php /usr/local/bin/composer.phar"
 export PATH="$PATH:$HOME/.composer/vendor/bin"
 
 DEFAULT_USER=iosif
--- sources/paths.sh
+++ sources/paths.sh
@@ -2,14 +2,13 @@
 alias www='cd ~/www'
 alias bitbucket='cd ~/www/bitbucket'
 alias github='cd ~/www/github'
-alias temper='cd ~/www/temper'
 
 alias toolbelt='cd ~/www/github/terminal-toolbelt'
 alias cogs='cd ~/www/bitbucket/cogs'
 
 ### OSX ###
 alias edit-hosts='nano /private/etc/hosts'
-alias fs_usage='sudo fs_usage'
+alias fs-usage='sudo fs_usage'
 
 ### Server ###
 alias srv-logs='cd /var/log/apache2'
