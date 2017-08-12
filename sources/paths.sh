4931bdc57b7c6d5f70e6480f7a5a0ec96c8578dd
Iosif Vigh <vighiosif@gmail.com>
2017-08-13 00:05:39 +0100

    [master] - updated locations

--- sources/paths.sh
+++ sources/paths.sh
@@ -1,15 +1,17 @@
 ### Projects ###
 alias www='cd ~/www'
 alias bitbucket='cd ~/www/bitbucket'
+alias iosifv='cd ~/www/bitbucket/iosifv'
 alias github='cd ~/www/github'
+alias toolbelt='cd ~/www/github/terminal-toolbelt'
 alias homestead='cd ~/www/soft/Homestead'
 alias start-homestead='cd ~/www/soft/Homestead/ && vagrant reload --provision'
 alias woooba='cd ~/www/woooba'
+alias clients='cd ~/www/woooba/clients'
+alias oddprophet='cd ~/www/woooba/clients/oddprophet'
 alias ysa='cd ~/www/woooba/clients/ysa'
 alias s2a='cd ~/www/woooba/clients/s2a'
-alias iosifv='cd ~/www/bitbucket/iosifv'
 
-alias toolbelt='cd ~/www/github/terminal-toolbelt'
 alias cogs='cd ~/www/bitbucket/cogs'
 
 ### OSX ###
