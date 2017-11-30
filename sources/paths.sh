f30cd2a115327b894298c5447f317b46d98c6c9a
Iosif <vighiosif@gmail.com>
2017-11-30 19:07:41 +0200

    [master] - added some paths

--- sources/paths.sh
+++ sources/paths.sh
@@ -1,17 +1,27 @@
-### Projects ###
+### Generic ###
 alias www='cd ~/www'
 alias bitbucket='cd ~/www/bitbucket'
+alias github='cd ~/www/github'
+
+### Personal projects ###
 alias cogs='cd ~/www/bitbucket/cogs'
 alias iosifv='cd ~/www/bitbucket/iosifv'
-alias github='cd ~/www/github'
 alias toolbelt='cd ~/www/github/terminal-toolbelt'
+
+### System stuff ###
 alias homestead='cd ~/www/soft/Homestead'
 alias start-homestead='cd ~/www/soft/Homestead/ && vagrant reload --provision'
+
+### Woooba projects ###
 alias woooba='cd ~/www/woooba'
+
+### Client projects ###
 alias clients='cd ~/www/woooba/clients'
 alias oddprophet='cd ~/www/woooba/clients/odd-prophet-api'
 alias ysa='cd ~/www/woooba/clients/ysa'
 alias s2a='cd ~/www/woooba/clients/s2a'
+alias doctors='cd ~/www/woooba/clients/doctors'
+alias teps='cd ~/www/woooba/clients/teps'
 
 ### Server ###
 alias srv-logs='cd /var/log/apache2'
--- sources/ssh.sh
+++ sources/ssh.sh
@@ -1,6 +1,4 @@
-# Push all changes to origin branch named as current branch
-# Ex: gpush "<<commit-message>>"
-#----------------------------
+
 function ssh-make {
   ssh-keygen -t rsa -b 4096 -C "$1"
 }
@@ -9,14 +7,19 @@ alias ssh-show='cat ~/.ssh/id_rsa.pub'
 alias ssh-copy='cat ~/.ssh/id_rsa.pub | pbcopy'
 alias ssh-list='ls -al ~/.ssh'
 
-# ssh-copy-id ioto@173.236.227.58
+# Personal
 alias sshto-dreamhost='ssh ioto@173.236.227.58'
+alias sshto-vultr='ssh forge@104.238.172.170'
+
+# Work - Misc
+alias sshto-selectron='ssh root@178.62.59.175'
+alias sshto-fresh='ssh forge@138.68.149.28'
+alias sshto-stale='ssh forge@178.62.6.15'
+
+# Work - Clients
 alias sshto-ysa='ssh forge@178.62.1.65'
 alias sshto-s2a='ssh forge@178.62.8.108'
 alias sshto-oddprophet='ssh forge@46.101.62.66'
-alias sshto-selectron='ssh root@178.62.59.175'
-alias sshto-vultr='ssh forge@104.238.172.170'
-alias sshto-fresh='ssh forge@138.68.149.28'
-alias sshto-stale='ssh forge@178.62.6.15'
 alias sshto-opentrack='ssh forge@139.59.190.154'
 alias sshto-teps='ssh forge@139.59.174.110'
+alias sshto-doctors='ssh forge@46.101.60.39'
