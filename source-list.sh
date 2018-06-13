e642b4b097d998411e870932237ab631650fc795
Iosif Vigh <dev@iosifv.com>
2018-06-13 14:31:18 +0100

    added a cool new welcome message

--- source-list.sh
+++ source-list.sh
@@ -3,6 +3,7 @@ SOURCES_PATH=$DIR_PATH_FOR_TOOLBELT/sources
 # Need to be first
 source $SOURCES_PATH/configs.sh
 source $SOURCES_PATH/colors.sh
+source $SOURCES_PATH/welcome.sh
 
 # All outhers
 source $SOURCES_PATH/apps.sh
@@ -13,5 +14,4 @@ source $SOURCES_PATH/system.sh
 source $SOURCES_PATH/tmux.sh
 source $SOURCES_PATH/ssh.sh
 source $SOURCES_PATH/fun.sh
-
 source $SOURCES_PATH/ubuntu.sh
---
+++ sources/welcome.sh
@@ -0,0 +1,2 @@
+echo " ---===[[[ ${C_YELLOW} Today is a great day to ${C_GREEN}HUSTLE${C_RESET}  ]]]===---"
+echo "                                  powered by ${C_YELLOW}Toolbelt${C_RESET}"
