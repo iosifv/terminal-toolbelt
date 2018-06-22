446220d52cbb46bba8df42ee9b7866cc2dbd8ac2
Iosif Vigh <dev@iosifv.com>
2018-06-22 09:22:22 +0300

    [master] - added a cool welcome text

--- sources/welcome.sh
+++ sources/welcome.sh
@@ -1,2 +1,7 @@
+#!/usr/bin/env bash
 echo " ---===[[[ ${C_YELLOW} Today is a great day to ${C_GREEN}HUSTLE${C_RESET}  ]]]===---"
 echo "                                  powered by ${C_YELLOW}Toolbelt${C_RESET}"
+echo ""
+php -r "echo 'PHP v' . substr(phpversion(), 0, 3);"
+echo ""
+cat /proc/meminfo | grep "MemFree" && uptime -p
