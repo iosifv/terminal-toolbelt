af7e37c091f8667365388476c46415c3aad4d97d
Iosif Vigh <dev@iosifv.com>
2018-09-10 11:24:42 +0100

    [master] - apt-update

--- sources/os-general.sh
+++ sources/os-general.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
+# Updates
+function apt-update {
+    sudo echo ""
+    echo "${C_YELLOW}==== Running apt-get ${C_GREEN}update${C_YELLOW} ====${C_RESET}"
+    sudo apt-get update
+    echo "${C_YELLOW}==== Running apt-get ${C_GREEN}upgrade${C_YELLOW} ====${C_RESET}"
+    sudo apt-get upgrade -y
+    echo "${C_YELLOW}==== Running apt-get ${C_GREEN}dist-upgrade${C_YELLOW} ====${C_RESET}"
+    sudo apt-get dist-upgrade -y
+    echo "${C_YELLOW}==== Running apt-get ${C_GREEN}autoremove${C_YELLOW} ====${C_RESET}"
+    sudo apt-get autoremove -y
+    echo "${C_YELLOW}==== Running apt-get ${C_GREEN}autoclean${C_YELLOW} ====${C_RESET}"
+    sudo apt-get autoclean -y
+    echo "${C_YELLOW}==== ${C_GREEN}Done! :)${C_YELLOW} ====${C_RESET}"
+}
+
 # Files
 #----------------------------
 function dir-sizes {
