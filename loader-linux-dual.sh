93190d2131004357ddad878662897c9530a7ffce
Iosif Vigh <dev@iosifv.com>
2018-12-10 21:50:04 +0000

    [master] - stuff

--- loader-linux-dual.sh
+++ loader-linux-dual.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo "."
 
-DIR_PATH_FOR_WWW="/home/iosif/www"
+DIR_PATH_FOR_WWW="/media/iosif/Vault/www"
 DIR_PATH_FOR_TOOLBELT="${DIR_PATH_FOR_WWW}/github/terminal-toolbelt"
 
-DIR_PATH_FOR_BIN="/usr/bin"
+DIR_PATH_FOR_BIN="/usr/local/bin"
 
 source $DIR_PATH_FOR_TOOLBELT/source-list.sh
 
--- sources/welcome.sh
+++ sources/welcome.sh
@@ -2,6 +2,6 @@
 echo " ---===[[[ ${C_YELLOW} Today is a great day to ${C_GREEN}HUSTLE${C_RESET}  ]]]===---"
 echo "                                  powered by ${C_YELLOW}Toolbelt${C_RESET}"
 echo ""
-php -r "echo 'PHP v' . substr(phpversion(), 0, 3);"
+# php -r "echo 'PHP v' . substr(phpversion(), 0, 3);"
 echo ""
 cat /proc/meminfo | grep "MemFree" && uptime -p
