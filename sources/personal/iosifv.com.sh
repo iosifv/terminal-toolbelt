e2302a1ba6385c2f70b2e94d6a32dbac3b75a94f
Iosif Vigh <dev@iosifv.com>
2019-03-22 10:27:57 +0000

    [master] - pcloud

--- sources/personal/iosifv.com.sh
+++ sources/personal/iosifv.com.sh
@@ -1,6 +1,7 @@
 alias iv-start='cd /home/ioto/www/iosifv && gtake master && npm run dev'
 alias iv-photos='cd /home/ioto/www/iosifv/storage/app/public/photos'
 alias iv-photos-backup='mv /home/ioto/www/iosifv/storage/app/public/photos /home/ioto/www/iosifv/storage/app/public/photos-bck'
-alias iv-photos-copy-from-pcloud='rclone copy pcloud:Fotografii/__iosifv.com /home/ioto/www/iosifv/storage/app/public/photos'
+alias iv-photos-pcloud-copy='rclone copy -v pcloud:Fotografii/__iosifv.com /home/ioto/www/iosifv/storage/app/public/photos'
+alias iv-photos-pcloud-replace='rm -rf /home/ioto/www/iosifv/storage/app/public/photos && rclone copy -v pcloud:Fotografii/__iosifv.com /home/ioto/www/iosifv/storage/app/public/photos'
 alias iv-log='cd /home/ioto/www/iosifv/storage/logs'
 alias iv-log-tail='tail -f /home/ioto/www/iosifv/storage/logs/laravel.log'
