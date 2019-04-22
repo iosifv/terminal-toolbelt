e2908b0150709b1cdd60a212f4d62b2aaeda4ec9
Iosif Vigh <dev@iosifv.com>
2019-04-22 11:51:28 +0200

    [master] - renamed utils

--- sources/bundle/util.sh
+++ sources/bundle/util.sh
@@ -5,7 +5,9 @@ alias show-my-ip='curl -4 icanhazip.com'
 # Super useful weather info because asking Siri or Google is mainstream
 #----------------------------
 alias weather='curl wttr.in/London'
+alias weather-london='curl wttr.in/London'
 alias weather-cluj='curl wttr.in/Cluj'
+alias weather-wroclaw='curl wttr.in/Wroclaw'
 
 
 # Read markdown files nicely
