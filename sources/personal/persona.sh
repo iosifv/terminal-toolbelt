73b711218645960152954bcea2fff9271828ee58
Iosif Vigh <dev@iosifv.com>
2019-05-10 16:52:31 +0100

    [master] - persona-list-networks

--- sources/personal/persona.sh
+++ sources/personal/persona.sh
@@ -9,10 +9,23 @@ alias persona-status=''
 function persona-listConfig-personaCore {
     print-status 'Displaying files from ~/.config/persona-core/mainnet/'
     echo ''
-    print-status 'delegates.json'
+    print-status '[delegates.json]'
     cat ~/.config/persona-core/mainnet/delegates.json
-    print-status 'peers.json'
+    print-status '[peers.json]'
     cat ~/.config/persona-core/mainnet/peers.json
-    print-status 'plugins.js'
+    print-status '[plugins.js]'
     cat ~/.config/persona-core/mainnet/plugins.js
+}
+
+function persona-listConfig-networks {
+    print-status 'Displaying files from ~/persona-core/packages/crypto/dist/networks/mainnet/'
+    echo ''
+    print-status '[exceptions.json]'
+    cat ~/persona-core/packages/crypto/dist/networks/mainnet/exceptions.json
+    print-status '[genesisBlock.json]'
+    cat ~/persona-core/packages/crypto/dist/networks/mainnet/genesisBlock.json
+    print-status '[index.js]'
+    cat ~/persona-core/packages/crypto/dist/networks/mainnet/index.json
+    print-status '[network.json]'
+    cat ~/persona-core/packages/crypto/dist/networks/mainnet/network.json
 }
