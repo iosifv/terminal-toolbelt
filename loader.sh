a1af6202b44ab529ba33313967aaa780a1a2d0fc
Iosif V <dev@iosifv.com>
2019-05-10 15:47:03 +0000

    [master] - dos2unix

---
+++ loader.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+OS_NAME="ubuntu"
+DIR_PATH_FOR_WWW="/home/blacksmith"
+DIR_PATH_FOR_TOOLBELT="${DIR_PATH_FOR_WWW}/terminal-toolbelt"
+
+DIR_PATH_FOR_BIN="/usr/bin"
+
+source $DIR_PATH_FOR_TOOLBELT/source-list.sh
+
+echo "."
