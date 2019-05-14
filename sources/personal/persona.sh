2afbcc01051eba06b508de25734eda2c28923e42
Iosif Vigh <dev@iosifv.com>
2019-05-14 19:37:55 +0100

    [master] - persona pygmentize

--- sources/personal/persona.sh
+++ sources/personal/persona.sh
@@ -8,38 +8,30 @@ function persona-listConfig-personaCore {
     print-status 'Displaying files from ~/.config/persona-core/mainnet/'
     echo ''
     print-status '[delegates.json]'
-    cat ~/.config/persona-core/mainnet/delegates.json
+    pygmentize -O style=colorful -l json ~/.config/persona-core/mainnet/delegates.json
     print-status '[peers.json]'
-    cat ~/.config/persona-core/mainnet/peers.json
+    pygmentize -O style=colorful -l json ~/.config/persona-core/mainnet/peers.json
     print-status '[plugins.js]'
-    cat ~/.config/persona-core/mainnet/plugins.js
+    pygmentize -O style=colorful -l json ~/.config/persona-core/mainnet/plugins.js
 }
 
 function persona-listConfig-networks {
     print-status 'Displaying files from ~/persona-core/packages/crypto/dist/networks/mainnet/'
     echo ''
     print-status '[exceptions.json]'
-    cat ~/persona-core/packages/crypto/dist/networks/mainnet/exceptions.json
+    pygmentize -O style=colorful -l json ~/persona-core/packages/crypto/dist/networks/mainnet/exceptions.json
     print-status '[genesisBlock.json]'
-    cat ~/persona-core/packages/crypto/dist/networks/mainnet/genesisBlock.json
+    pygmentize -O style=colorful -l json ~/persona-core/packages/crypto/dist/networks/mainnet/genesisBlock.json
     print-status '[index.js]'
-    cat ~/persona-core/packages/crypto/dist/networks/mainnet/index.json
+    pygmentize -O style=colorful -l json ~/persona-core/packages/crypto/dist/networks/mainnet/index.json
     print-status '[network.json]'
-    cat ~/persona-core/packages/crypto/dist/networks/mainnet/network.json
+    pygmentize -O style=colorful -l json ~/persona-core/packages/crypto/dist/networks/mainnet/network.json
 }
 
-function persona-listConfig-tbw {
-    print-status 'Displaying files from cat ~/tbw/config/'
-    echo ''
-    print-status '[config.json]'
-    cat cat ~/tbw/config/config.json
-    print-status '[pool.json]'
-    cat cat ~/tbw/config/pool.json
-}
+alias persona-listConfig-tbw='pygmentize -O style=colorful -l python ~/core2_tbw/core/config'
+alias persona-listConfig-databses='psql -l'
+alias persona-listConfig-secret='pygmentize -O style=colorful -l json ~/.config/persona-core/mainnet/delegates.json'
 
-alias persona-relay-start='~/core-control/ccontrol.sh start relay'
-
-alias persona-show-databses='psql -l'
-alias persona-payments-manual='cd ~/tbw/core_v1 && python3 ~/tbw/core_v1/tbw.py'
-alias persona-payments-start='pm2 start ~/tbw/core_v1/apps.json'
-alias persona-payments-stop='pm2 stop ~/tbw/core_v1/apps.json'
+alias persona-start-relay='~/core-control/ccontrol.sh start relay'
+alias persona-start-forger='~/core-control/ccontrol.sh start forger'
+alias persona-tbw='cd ~/core2_tbw/ && bash tbw.sh'
