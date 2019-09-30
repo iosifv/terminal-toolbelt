4a56a695b972486319c86280c69f485b928cecd4
Iosif V <dev@iosifv.com>
2019-09-30 03:06:53 +0100

    [master] - tmux-config

--- installers/install-02-my-projects.sh
+++ installers/install-02-my-projects.sh
@@ -64,10 +64,15 @@ git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/theme
 # nerd-fonts
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
- 
 # Install more fonts
-# Install nanorc
-# Install tmux
+
+# tmux-config
+cd ~
+git clone git@github.com:gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp ~/www/github/terminal-toolbelt/installers/extras/tmux-conf/.tmux.conf.local ~/.tmux.conf.local 
+
+
 
 
 # ====== Projects SetUp ========
