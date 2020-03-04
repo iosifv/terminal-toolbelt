622e381162f71318fe5f2e8cc88ca44d3a74478b
Iosif V <dev@iosifv.com>
2020-03-04 13:40:43 +0000

    [master] - stuff

--- sources/alpha-configs.sh
+++ sources/alpha-configs.sh
@@ -15,3 +15,8 @@ export PATH=/home/iosif/.composer/vendor/bin:$PATH
 export PATH=$PATH:~/bin
 
 # export TERM="xterm-256color"
+
+# NVM configs
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
